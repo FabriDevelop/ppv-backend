9c43b64654d26c9ed12322d00dad6dbd96fa25be
FabriDevelop <cfar.dev@gmail.com>
2022-05-20 12:47:01 -0600

    🧑 Editar Cliente - Realizado

--- src/routes/clientes.router.ts
+++ src/routes/clientes.router.ts
@@ -1,7 +1,11 @@
 import express, { NextFunction, Request, Response } from 'express'
 import ICliente from '../interfaces/ICliente'
 import validatorHandler from '../middleware/validator.handler'
-import { crearClienteSchema } from '../schemas/cliente.schema'
+import {
+  crearClienteSchema,
+  obtenerClienteSchema,
+  editarClienteSchema
+} from '../schemas/cliente.schema'
 import ClienteService from '../services/cliente.service'
 
 const router = express.Router()
@@ -18,6 +22,19 @@ router.post(
     } catch (error) {
       next(error)
     }
+  }
+)
+
+router.patch(
+  '/:id',
+  validatorHandler(obtenerClienteSchema, 'params'),
+  validatorHandler(editarClienteSchema, 'body'),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      const body = req.body
+      res.status(201).json(await service.update(Number(id), body))
+    } catch (error) {}
   }
 )
 
--- src/services/cliente.service.ts
+++ src/services/cliente.service.ts
@@ -4,9 +4,21 @@ import ICliente from '../interfaces/ICliente'
 const prisma = new PrismaClient()
 
 class ClienteService {
-  async create(data: any): Promise<ICliente> {
+  async create(data: ICliente): Promise<ICliente> {
     const clienteNuevo = await prisma.cliente.create({ data })
     return clienteNuevo
+  }
+
+  async update(id: number, cambios: ICliente) {
+    const clienteActualizado = await prisma.cliente.update({
+      where: {
+        id
+      },
+      data: {
+        ...cambios
+      }
+    })
+    return clienteActualizado
   }
 }
 
